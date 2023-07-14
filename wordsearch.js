b889d14b4628fbf00955456bb06962e52830d2b3
Sharleen Beniamin <sharleenb16@gmail.com>
2023-07-14 23:09:44 +0000

    add tests for vertical search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function (letters) {
+  for (let i = 0; i < letters.length; i++) {
+    for (let j = 0; j < i; j++) {
+      const temp = letters[i][j];
+      letters[i][j] = letters[j][i];
+      letters[j][i] = temp;
     }
-}
+  }
+  return letters;
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  const verticalJoin = transpose(letters).map((ls) => ls.join(""));
+  for (l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
